87bb67064536984cb575d615071ad8a52ff33623
Illia Antonenko <antonenko.red@gmail.com>
2022-06-07 23:42:58 +0200

    feat (dotenv): added env file support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://admin:MS0l3mPbdtjlpFwa@cluster0.od4pt.mongodb.net/best-chat?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.od4pt.mongodb.net/best-chat?retryWrites=true&w=majority`,
     ),
     ChatModule,
     UserModule,
