47a40f05d4fe683ff767feb9edb74f62e205992d
Illia Antonenko <antonenko.red@gmail.com>
2023-04-04 18:38:09 +0300

    feat(chat): chat user relation

--- backend/src/chat/chat.schema.ts
+++ backend/src/chat/chat.schema.ts
@@ -1,17 +1,26 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Transform } from 'class-transformer';
 import mongoose, { Document } from 'mongoose';
 import { User } from 'src/user/user.schema';
+import { Message } from './messages/message.schema';
 
 export type ChatDocument = Chat & Document;
 
 @Schema()
 export class Chat {
+  @Transform((value) => value.obj._id.toString())
+  _id: string;
   @Prop({
     type: [
       { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     ],
   })
-  partisipants: User[];
+  participantList: User[];
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
+  })
+  messageList?: Message[];
 }
 
 export const ChatSchema = SchemaFactory.createForClass(Chat);
